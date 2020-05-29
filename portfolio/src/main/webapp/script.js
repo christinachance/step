82f483d2012da87b20557c964b19265deed8f16c
Christina <christinachance315@gmail.com>
2020-05-29 09:09:27 -0400

    fixed spacing, renamede varibales, and editsed js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -21,12 +21,11 @@ showSlides();
 
 
 function showSlides(){
-    var i;
+    var currentImgIndex;
     var slides = document.getElementsByClassName("mySlides");
-    for(i=0; i<slides.length;i++){
-        slides[i].style.display="none";
+    for(currentImgIndex=0; currentImgIndex<slides.length;currentImgIndex++){
+        slides[currentImgIndex].style.display="none";
     }
-    console.log(slides.length);
     slideIndex++;
     if(slideIndex> slides.length){
         slideIndex=1;
