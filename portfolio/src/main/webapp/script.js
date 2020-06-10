b460f5e8d6c8674c9ebf945a79fb456af0a0d743
Christina <christinachance315@gmail.com>
2020-06-10 15:17:50 +0000

    added Map API

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -56,3 +56,66 @@ function getUserLogin() {
         }
     });
 }
+
+var map;
+var markers = [];
+
+function initMap(){
+    var hartford = {lat: 41.78, lng: -72.69};
+    map = new google.maps.Map(
+        document.getElementById('map'),
+        {center: hartford, zoom: 5, mapTypeId: 'terrain'});
+    var atlanta = {lat: 33.79, lng: -84.32};
+    var jamaica = {lat: 18.39, lng: -77.39};
+    var munnar = {lat: 10.079, lng: 77.062};
+    var hightstown = {lat: 40.267, lng: -74.52};
+    var disney = {lat: 28.38, lng: -81.56};
+    var sunnyvale = {lat: 37.403, lng: -122.03};
+    var manhattan = {lat: 40.71, lng: -74.002};
+    var dc = {lat: 38.92, lng: -77.02};
+    var niagra = {lat:43.25, lng:-79.079};
+    addMarker(hartford);
+    addMarker(atlanta);
+    addMarker(jamaica);
+    addMarker(munnar);
+    addMarker(hightstown);
+    addMarker(disney);
+    addMarker(sunnyvale);
+    addMarker(manhattan);
+    addMarker(dc);
+    addMarker(niagra);
+
+    google.maps.event.addListener(map, 'click', function(event) {
+            addMarker(event.latLng, "purple");
+    });
+}
+
+function addMarker(location, color="blue"){
+    var marker = new google.maps.Marker({
+        position: location,
+        map:map,
+        icon:{ url: "http://maps.google.com/mapfiles/ms/icons/"+color+"-dot.png"}
+    });
+    markers.push(marker);
+}
+
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+function clearMarkers() {
+    setMapOnAll(null);
+}
+
+function showMarkers() {
+    setMapOnAll(map);
+}
+
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
