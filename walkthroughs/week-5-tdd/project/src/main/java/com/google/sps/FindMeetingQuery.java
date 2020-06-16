68022585097c8a5380b5ef37334876c1a03e2742
Christina <christinachance315@gmail.com>
2020-06-16 20:11:38 +0000

    implemented query() and added tests for optional attendees, also added image to portfolio

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,161 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.*; 
+
+
+/*
+for the coding challenge, keep the times for the mandatory, and then create a seperate scedule with possbible times, make it a 
+dictionary so we can keep track of how many people can attend from the optional that time and then school teh times withe the largest number of people
+*/
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+
+
+  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request){
+    ArrayList<TimeRange> busyMandatory = new ArrayList<>();
+    ArrayList<TimeRange> busyOptional = new ArrayList<>();
+    ArrayList<TimeRange> availableMandatory = new ArrayList<>();
+    ArrayList<TimeRange> availableOptional = new ArrayList<>();
+    ArrayList<TimeRange> available = new ArrayList<>();
+
+    //Mandatory Attendees
+    // adds all time ranges when attendees are busy
+    for(Event event : events){
+        for(String attendee : request.getAttendees()){
+            //adding time frame only if not in there and one attendee is also an attendee of event
+            if(event.getAttendees().contains(attendee) && !busyMandatory.contains(event.getWhen())){
+                busyMandatory.add(event.getWhen());
+                break;
+            }
+        }
+    }
+
+    Collections.sort(busyMandatory, TimeRange.ORDER_BY_START);
+
+    //iterate over the list of busy and if an event is containd in another, remove it.
+    Iterator<TimeRange> iter = busyMandatory.iterator();
+    while(iter.hasNext()){
+        TimeRange current = iter.next();
+        for(int checkIndex=0; checkIndex <busyMandatory.size(); checkIndex++){
+            if(current.start() > busyMandatory.get(checkIndex).start() && current.end() < busyMandatory.get(checkIndex).end()){
+                iter.remove();
+            }
+        }
+    }
+
+    //Optional Attendees
+    for(Event event : events){
+            for(String attendee : request.getOptionalAttendees()){
+                if(event.getAttendees().contains(attendee) && !busyMandatory.contains(event.getWhen())){
+                    busyOptional.add(event.getWhen());
+                    break;
+                }
+            }
+        }
+
+        Collections.sort(busyOptional, TimeRange.ORDER_BY_START);
+
+        Iterator<TimeRange> iterOptional = busyOptional.iterator();
+        while(iter.hasNext()){
+            TimeRange current = iterOptional.next();
+            for(int checkIndex=0; checkIndex <busyOptional.size(); checkIndex++){
+                if(current.start() > busyOptional.get(checkIndex).start() && current.end() < busyOptional.get(checkIndex).end()){
+                    iterOptional.remove();
+                }
+            }
+        }
+
+    //checks availabilty of optional attendees
+    if(request.getAttendees().isEmpty() && !request.getOptionalAttendees().isEmpty()){
+        return getAvailability(busyOptional, request);
+
+    } else if (!request.getAttendees().isEmpty() && request.getOptionalAttendees().isEmpty()){
+        return getAvailability(busyMandatory, request);
+
+    } else {
+        availableMandatory = getAvailability(busyMandatory, request);
+        availableOptional = getAvailability(busyOptional, request);
+
+        if(availableMandatory.isEmpty() && !availableOptional.isEmpty()){
+            return availableOptional;
+        }else if (availableOptional.isEmpty() && !availableMandatory.isEmpty()){
+            return availableMandatory;
+        }else{
+            for(TimeRange optional : availableOptional){
+                for(TimeRange mandatory : availableMandatory){
+                    if(mandatory.contains(optional)){
+                        if(optional.duration() == Math.min(optional.duration(), mandatory.duration()) && !available.contains(TimeRange.fromStartDuration(optional.start(), optional.duration()))){
+                            available.add(TimeRange.fromStartDuration(optional.start(), optional.duration()));
+                        }else{
+                            if(!available.contains(TimeRange.fromStartDuration(mandatory.start(), mandatory.duration()))){
+                                available.add(TimeRange.fromStartDuration(mandatory.start(), mandatory.duration()));
+                            }
+                        }
+                    }
+                    if(mandatory.overlaps(optional) && request.getDuration() <= (Math.min(optional.end(), mandatory.end())- Math.max(optional.start(), mandatory.start())-1)){
+                        if(!available.contains(TimeRange.fromStartEnd(Math.max(optional.start(), mandatory.start()), Math.min(optional.end(), mandatory.end()), false))){
+                            available.add(TimeRange.fromStartEnd(Math.max(optional.start(), mandatory.start()), Math.min(optional.end(), mandatory.end()), false));
+                        }
+                    }
+                }
+            }
+            if(available.isEmpty()){
+                return availableMandatory;
+            }else{
+                return available;
+            }
+
+        }
+    }
   }
+
+  private ArrayList<TimeRange> getAvailability(ArrayList<TimeRange> busyTimes, MeetingRequest request){
+    ArrayList<TimeRange> availableTimes = new ArrayList<>();
+
+    if(request.getDuration() > TimeRange.END_OF_DAY){
+        return availableTimes;
+    }
+    
+    if(busyTimes.isEmpty()){
+        availableTimes.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.END_OF_DAY, true));
+        return availableTimes;
+    }
+
+    if(busyTimes.size()==1){
+        if(busyTimes.get(0) == TimeRange.WHOLE_DAY){
+            return availableTimes;
+        }else{
+            availableTimes.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, busyTimes.get(0).start(), false));
+            availableTimes.add(TimeRange.fromStartEnd(busyTimes.get(busyTimes.size()-1).end(), TimeRange.END_OF_DAY, true));
+            return availableTimes;
+        }
+    }
+
+    if(busyTimes.get(0).start()!= 0){
+        availableTimes.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, busyTimes.get(0).start(), false));
+    }
+
+    //adds possible time range for the avaiable times when none of the groups meetings overlap and ensures that the duration in available  times is more or equivalent to requested duration
+    for( int timeIndex = 1; timeIndex < busyTimes.size(); timeIndex++){
+        if(!busyTimes.get(timeIndex).overlaps(busyTimes.get(timeIndex-1)) && request.getDuration()<=(busyTimes.get(timeIndex).start()) - busyTimes.get(timeIndex-1).end()){
+            if(busyTimes.get(timeIndex).end() < busyTimes.get(timeIndex-1).end()){
+                availableTimes.add(TimeRange.fromStartEnd(busyTimes.get(timeIndex).end(), busyTimes.get(timeIndex-1).end(), false));
+            }else{
+                availableTimes.add(TimeRange.fromStartEnd(busyTimes.get(timeIndex-1).end(), busyTimes.get(timeIndex).start(), false));
+            }
+        }
+    }
+
+    //adds all the time after the last meeting of the group of attendees
+    if(busyTimes.get(busyTimes.size()-1).end()!= TimeRange.END_OF_DAY &&  (TimeRange.END_OF_DAY - busyTimes.get(busyTimes.size()-1).end()-1) > 0 ){
+        availableTimes.add(TimeRange.fromStartEnd(busyTimes.get(busyTimes.size()-1).end(), TimeRange.END_OF_DAY, true));
+    }
+
+    return availableTimes;
+  }
+
 }
--- walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
+++ walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
@@ -34,6 +34,9 @@ public final class FindMeetingQueryTest {
   // Some people that we can use in our tests.
   private static final String PERSON_A = "Person A";
   private static final String PERSON_B = "Person B";
+  private static final String PERSON_C = "Person C";
+  private static final String PERSON_D = "Person D";
+
 
   // All dates are the first day of the year 2020.
   private static final int TIME_0800AM = TimeRange.getTimeInMinutes(8, 0);
@@ -41,8 +44,11 @@ public final class FindMeetingQueryTest {
   private static final int TIME_0900AM = TimeRange.getTimeInMinutes(9, 0);
   private static final int TIME_0930AM = TimeRange.getTimeInMinutes(9, 30);
   private static final int TIME_1000AM = TimeRange.getTimeInMinutes(10, 0);
+  private static final int TIME_1030AM = TimeRange.getTimeInMinutes(10, 30);
   private static final int TIME_1100AM = TimeRange.getTimeInMinutes(11, 00);
+  private static final int TIME_1130AM = TimeRange.getTimeInMinutes(11, 30);
 
+  private static final int DURATION_15_MINUTES = 15;
   private static final int DURATION_30_MINUTES = 30;
   private static final int DURATION_60_MINUTES = 60;
   private static final int DURATION_90_MINUTES = 90;
@@ -107,15 +113,21 @@ public final class FindMeetingQueryTest {
         new Event("Event 1", TimeRange.fromStartDuration(TIME_0800AM, DURATION_30_MINUTES),
             Arrays.asList(PERSON_A)),
         new Event("Event 2", TimeRange.fromStartDuration(TIME_0900AM, DURATION_30_MINUTES),
-            Arrays.asList(PERSON_B)));
+            Arrays.asList(PERSON_B)),
+        new Event("Event 3", TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.END_OF_DAY, true), 
+            Arrays.asList(PERSON_C)),
+        new Event("Event 4", TimeRange.fromStartDuration(TIME_0830AM, DURATION_30_MINUTES),
+            Arrays.asList(PERSON_D)));
 
     MeetingRequest request =
         new MeetingRequest(Arrays.asList(PERSON_A, PERSON_B), DURATION_30_MINUTES);
+    // request.addOptionalAttendee(PERSON_C);
+    request.addOptionalAttendee(PERSON_D);
 
     Collection<TimeRange> actual = query.query(events, request);
     Collection<TimeRange> expected =
         Arrays.asList(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_0800AM, false),
-            TimeRange.fromStartEnd(TIME_0830AM, TIME_0900AM, false),
+            // TimeRange.fromStartEnd(TIME_0830AM, TIME_0900AM, false),
             TimeRange.fromStartEnd(TIME_0930AM, TimeRange.END_OF_DAY, true));
 
     Assert.assertEquals(expected, actual);
@@ -212,9 +224,12 @@ public final class FindMeetingQueryTest {
         new Event("Event 1", TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_0830AM, false),
             Arrays.asList(PERSON_A)),
         new Event("Event 2", TimeRange.fromStartEnd(TIME_0900AM, TimeRange.END_OF_DAY, true),
-            Arrays.asList(PERSON_A)));
+            Arrays.asList(PERSON_A)),
+        new Event("Event 3", TimeRange.fromStartDuration(TIME_0830AM, DURATION_15_MINUTES),
+            Arrays.asList(PERSON_B)));
 
     MeetingRequest request = new MeetingRequest(Arrays.asList(PERSON_A), DURATION_30_MINUTES);
+    request.addOptionalAttendee(PERSON_B);
 
     Collection<TimeRange> actual = query.query(events, request);
     Collection<TimeRange> expected =
@@ -270,5 +285,44 @@ public final class FindMeetingQueryTest {
 
     Assert.assertEquals(expected, actual);
   }
-}
 
+  @Test
+  public void NoMandatoryWithGaps(){
+    //Request meeting with no mandatory atendees, but the optional ones have time to have the meeting.
+    Collection<Event> events = Arrays.asList(
+        new Event("Event 1", TimeRange.fromStartDuration(TIME_0930AM, DURATION_1_HOUR),
+            Arrays.asList(PERSON_A)),
+        new Event("Event 2", TimeRange.fromStartDuration(TIME_1100AM, DURATION_30_MINUTES),
+            Arrays.asList(PERSON_B)));
+
+    MeetingRequest request = new MeetingRequest(NO_ATTENDEES, DURATION_30_MINUTES);
+    request.addOptionalAttendee(PERSON_A);
+    request.addOptionalAttendee(PERSON_B);
+
+    Collection<TimeRange> actual = query.query(events, request);
+    Collection<TimeRange> expected =
+        Arrays.asList(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_0930AM, false),
+        TimeRange.fromStartEnd(TIME_1030AM, TIME_1100AM, false),
+        TimeRange.fromStartEnd(TIME_1130AM, TimeRange.END_OF_DAY, true));
+
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void NoMandatoryNoGaps(){
+    Collection<Event> events = Arrays.asList(
+        new Event("Event 1", TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_1030AM, false),
+            Arrays.asList(PERSON_A)),
+        new Event("Event 2", TimeRange.fromStartEnd(TIME_1030AM, TimeRange.END_OF_DAY, true),
+            Arrays.asList(PERSON_B)));
+    
+    MeetingRequest request = new MeetingRequest(NO_ATTENDEES, DURATION_90_MINUTES);
+    request.addOptionalAttendee(PERSON_A);
+    request.addOptionalAttendee(PERSON_B);
+
+    Collection<TimeRange> actual = query.query(events, request);
+    Collection<TimeRange> expected = Arrays.asList();
+
+    Assert.assertEquals(expected, actual);
+  }
+}
